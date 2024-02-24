90f32bc5aa586c5e92d0a7b5a2364bd575da6342
Dulmin Kasthuriarachchi <kasthuriarachchi2001@gmail.com>
2024-02-25 00:32:20 +0530

    db connection

---
+++ src/main/java/lk/ijse/carservicesystem/db/DbConnection.java
@@ -0,0 +1,24 @@
+package lk.ijse.carservicesystem.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    private static DbConnection dbConnection;
+    private Connection connection;
+
+    private DbConnection() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/car",
+                "root",
+                "##dulmin10146");
+    }
+    public static DbConnection getInstance() throws SQLException {
+        return (null == dbConnection) ? dbConnection = new DbConnection() : dbConnection;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+}
